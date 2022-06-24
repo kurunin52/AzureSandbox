bc727bfaf4e22c5c91c60bae5ca01cf523e0aad8
kurunin52 <33592045+kurunin52@users.noreply.github.com>
2022-06-24 16:54:33 +0900

    Update AboutView.vue

--- sample_front_end_application/src/views/AboutView.vue
+++ sample_front_end_application/src/views/AboutView.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div>
+      <p>
+        Tempor et accusam dolor amet stet vel ipsum tempor labore. Eos ipsum voluptua kasd quis duis consetetur amet voluptua ipsum volutpat at magna eu dolor et doming eos. Aliquyam vero molestie feugait. Te dolor magna. Dolore aliquam stet exerci eirmod vel et eros eu diam at consetetur rebum sed kasd tempor esse. Facer accumsan ipsum ullamcorper lorem congue amet et invidunt takimata magna stet sit stet et. Est at eos gubergren tempor est erat. Sadipscing ullamcorper dolor sadipscing sit elit vero eos consetetur nulla takimata facer rebum et dignissim ea. Duo sea stet eirmod wisi eirmod stet iriure eleifend accusam. Augue at ea ut. Ipsum eu clita consequat takimata justo et labore ex esse nulla nonumy. Ipsum diam est sea. Aliquip ipsum ut labore sit blandit cum dolore justo augue eirmod vero. Clita iusto erat consequat et no lorem.
+      </p>
+      <p>
+        Eirmod clita liber diam aliquyam vero consequat ea aliquyam magna nam no gubergren eirmod sadipscing ea lorem sadipscing. Vero illum takimata stet eos nonumy nonummy volutpat vel kasd est nonummy no duo liber gubergren sanctus imperdiet. Eirmod eu dolore luptatum eu takimata et facer invidunt wisi invidunt. Stet lorem sit vero lorem magna amet diam eirmod. Et nonumy accusam voluptua justo duis sed nonumy sed et velit. Vero molestie clita duo sadipscing. Dolor dolores eu ipsum kasd. Sed et sea gubergren sed erat ullamcorper rebum. Accusam ipsum dolor rebum tation et sadipscing lorem stet sanctus. Amet sed at vel ea eos et sadipscing amet velit eirmod hendrerit sed lorem labore lorem lorem ea dolore. Dolore iusto et soluta duis accusam.
+      </p>
+      <p>
+        Labore diam justo dolores sadipscing amet enim. In amet kasd no at minim. Nonumy labore clita stet lobortis invidunt duo nonumy molestie aliquyam sanctus commodo imperdiet invidunt at sed consetetur dolor tempor. Dolore odio esse adipiscing. Diam ipsum adipiscing vero dolor eos feugiat justo cum invidunt illum aliquam eros rebum dolore. Nonumy cum tempor tempor erat duo sed lorem eum dolores eirmod lorem soluta praesent dolore. Amet dolor illum et voluptua kasd iriure takimata imperdiet vel sadipscing justo facilisi kasd consequat feugiat. Nibh stet justo ut vel lorem sadipscing at et.
+      </p>
+      <p>
+        Accumsan vero dolore sit amet possim sea. Eleifend te sadipscing clita eos magna nostrud ea at nonummy. Vel amet amet veniam eos possim et sed autem sadipscing.
+      </p>
+    </div>
   </div>
 </template>
